c2bd525ea8c12492928a1572ce0bca8a50343278
ErgothDK <ergothdk@gmail.com>
2023-10-16 13:42:54 -0600

    Added css modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
-
+import React from "react";
 
 function App() {
   return (
     <div>
-
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label>Username</label>
+          <input type="text" onChange={txtUsernameChangeHandler} />
+        </div>
+        <div>
+          <label>Age (Years)</label>
+          <input type="number" onChange={txtAgeChangeHandler} />
+        </div>
+        <button type="submit">Add User</button>
+      </form>
+      <div>
+        <ul>
+          <li>Max (31 years old)</li>
+        </ul>
+      </div>
     </div>
   );
 }
